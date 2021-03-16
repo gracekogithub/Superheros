9bedac8536c5c1bbe6dbb9375b9dfd930722026f
gracekogithub <graceko02011972@gmail.com>
2021-03-16 11:55:41 -0700

    created superhero class

---
+++ SuperheroProject/Models/Superhero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperheroProject.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public string Name { get; set; }
+        public string Ego { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+
+
+    }
+}
